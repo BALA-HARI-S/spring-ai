5a836d57e3cb9eac63121e695f3008b7c9230f58
BALA-HARI-S <balahari754@gmail.com>
2024-06-26 18:00:22 +0530

    update demo controller

--- demo-ai/src/main/java/ai/spring/demo_ai/controller/DemoController.java
+++ demo-ai/src/main/java/ai/spring/demo_ai/controller/DemoController.java
@@ -10,17 +10,38 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/ai")
 public class DemoController {
 
+    // client interface that interacts with a chatbot or AI service to process and respond to user inputs.
     private final ChatClient chatClient;
 
     public DemoController(ChatClient.Builder chatClientBuilder) {
         this.chatClient = chatClientBuilder.build();
     }
 
+    @GetMapping("/prompt-user/youtube")
+    public String call(@RequestParam(value = "genre", defaultValue = "tech") String genre) {
 
-    @GetMapping("/joke")
-    public String call(@RequestParam(value = "message" , defaultValue = "tell me a simple joke") String message) {
-        return this.chatClient.prompt()
-                .user(message)
-                .call()
-                .content();    }
+        String message = """
+                List 10 of the most popular YouTubers in {genre} along with their current subscriber counts. If you don't know
+                the answer , just say "I don't know".
+                """;
+
+        return this.chatClient.prompt().user(u -> u.text(message).param("genre", genre)) // sets the text of the prompt to the multi-line string message
+                .call() // send the prompt to the AI and gets the response back
+                .content(); // extract the output
+    }
+
+//    @GetMapping("/prompt-user/joke")
+//    public String call(@RequestParam(value = "genre", defaultValue = "tell me a simple joke") String genre) {
+//
+//        String message = """
+//            List 10 of the most popular YouTubers in {genre} along with their current subscriber counts. If you don't know
+//            the answer , just say "I don't know".
+//            """;
+//        PromptTemplate promptTemplate = new PromptTemplate(message);
+//        Prompt prompt = promptTemplate.create(Map.of("genre",genre));
+//        return this.chatClient.prompt(prompt)
+//                .call()
+//                .content();
+//    }
+
 }
