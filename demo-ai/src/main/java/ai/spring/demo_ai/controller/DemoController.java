b25f066b8f367cd186a2115b86f423b216857a14
BALA-HARI-S <balahari754@gmail.com>
2024-07-01 10:56:04 +0530

    update DemoController & add RagController

--- demo-ai/src/main/java/ai/spring/demo_ai/controller/DemoController.java
+++ demo-ai/src/main/java/ai/spring/demo_ai/controller/DemoController.java
@@ -2,6 +2,10 @@ package ai.spring.demo_ai.controller;
 
 import ai.spring.demo_ai.entity.Author;
 import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.ai.chat.messages.Message;
+import org.springframework.ai.chat.messages.UserMessage;
+import org.springframework.ai.chat.prompt.Prompt;
+import org.springframework.ai.chat.prompt.SystemPromptTemplate;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.core.io.Resource;
@@ -12,6 +16,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.io.IOException;
 import java.nio.charset.Charset;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
@@ -32,7 +37,7 @@ public class DemoController {
     @Value("classpath:/prompts/map-prompt.st")
     private Resource mapPrompt;
 
-    @Value("classpath:/prompts/breezeware.st")
+    @Value("classpath:/prompts/breezeware-prompt.st")
     private Resource breezewarePrompt;
 
     @Value("classpath:/docs/breezeware.txt")
@@ -99,19 +104,22 @@ public class DemoController {
     // Prompt stuffing
     @GetMapping("/prompt/stuff")
     public String getBreezewareInfo(
-            @RequestParam(name = "message", defaultValue = "Tell me about Breezeware IT company") String message,
+            @RequestParam(name = "message", defaultValue = "Tell me about Breezeware IT company's 'Dynamo Platform'") String message,
             @RequestParam(name = "stuffit", defaultValue = "false") boolean stuffit
     ) throws IOException {
         String breezeware = breezewareDocs.getContentAsString(Charset.defaultCharset());
-        System.out.printf("Sports: %s%n", breezeware);
-        return chatClient.prompt()
-                .user(u -> {
-                    u.text(breezewarePrompt);
-                    u.param("question", message);
-                    u.param("context", stuffit ? breezeware : "");
-                })
+        System.out.printf("Breezeware: %s%n", breezeware);
+
+        List<Message> messages = new ArrayList<>();
+        UserMessage userMessage = new UserMessage(message);
+        messages.add(userMessage);
+        Message systemMessage = new SystemPromptTemplate(breezewarePrompt).createMessage(Map.of("context", breezeware));
+        messages.add(systemMessage);
+        Prompt prompt = new Prompt(messages);
+        return chatClient.prompt(prompt)
                 .call()
                 .content();
+
     }
 
 }
